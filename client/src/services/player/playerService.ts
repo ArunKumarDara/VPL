ccc5b658e4283cc13f5ba380f5973c943c8e82ac
Arun Kumar <mymailarun32@gmail.com>
2026-05-20 19:17:19 +0530

    create api service for the player

--- client/src/services/player/playerService.ts
+++ client/src/services/player/playerService.ts
@@ -1,11 +1,61 @@
-import { registerPlayerApi, RegisterPlayerResponse } from "@/api/playerApi";
+// src/services/player/playerService.ts
+
+import {
+  registerPlayerApi,
+  getAllPlayersApi,
+  getSinglePlayerApi,
+  updatePlayerApi,
+  deletePlayerApi,
+  CommonResponse,
+  RegisterPlayerResponse,
+  UpdatePlayerResponse,
+  GetAllPlayersResponse,
+  GetSinglePlayerResponse,
+} from "@/api/playerApi";
 
 // ======================================================
-// SERVICE
+// REGISTER PLAYER
 // ======================================================
 
 export const registerPlayer = async (
   data: FormData,
 ): Promise<RegisterPlayerResponse> => {
   return await registerPlayerApi(data);
+};
+
+// ======================================================
+// GET ALL PLAYERS
+// ======================================================
+
+export const getAllPlayers = async (): Promise<GetAllPlayersResponse> => {
+  return await getAllPlayersApi();
+};
+
+// ======================================================
+// GET SINGLE PLAYER
+// ======================================================
+
+export const getSinglePlayer = async (
+  id: string,
+): Promise<GetSinglePlayerResponse> => {
+  return await getSinglePlayerApi(id);
+};
+
+// ======================================================
+// UPDATE PLAYER
+// ======================================================
+
+export const updatePlayer = async (
+  id: string,
+  data: FormData,
+): Promise<UpdatePlayerResponse> => {
+  return await updatePlayerApi(id, data);
+};
+
+// ======================================================
+// DELETE PLAYER
+// ======================================================
+
+export const deletePlayer = async (id: string): Promise<CommonResponse> => {
+  return await deletePlayerApi(id);
 };
