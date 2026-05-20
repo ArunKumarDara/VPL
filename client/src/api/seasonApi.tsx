c6d8fc71746f641a493c224f37a02f6f45649bf7
Arun Kumar <mymailarun32@gmail.com>
2026-05-20 19:15:30 +0530

    create api for season

--- client/src/api/seasonApi.tsx
+++ client/src/api/seasonApi.tsx
@@ -6,17 +6,70 @@ import api from "./axios";
 // TYPES
 // ======================================================
 
+export type Owner = {
+    _id: string;
+    name: string;
+    email?: string;
+    mobile?: string;
+    profileImage?: string;
+    createdAt?: string;
+    updatedAt?: string;
+};
+
+export type Team = {
+    _id: string;
+    name: string;
+    shortName?: string;
+    logo?: string;
+    createdAt?: string;
+    updatedAt?: string;
+};
+
+export type Player = {
+    _id: string;
+    name: string;
+    role?: string;
+    playerType?: string;
+    basePrice?: number;
+    profileImage?: string;
+    createdAt?: string;
+    updatedAt?: string;
+};
+
+// ======================================================
+// SEASON TYPE
+// ======================================================
+
 export type Season = {
     _id: string;
+
     title: string;
+
     year: number;
+
     auctionDate?: string;
+
     tournamentStartDate?: string;
+
     tournamentEndDate?: string;
+
     status: "UPCOMING" | "LIVE" | "COMPLETED";
+
+    // RELATIONS
+
+    registeredPlayers: Player[];
+
+    owners: Owner[];
+
+    teams: Team[];
+
+    players: Player[];
+
     createdAt: string;
+
     updatedAt: string;
 };
+
 
 // ======================================================
 // GET ALL SEASONS
@@ -26,4 +79,12 @@ export const getAllSeasonsApi = async (): Promise<Season[]> => {
     const response = await api.get("/seasons");
 
     return response.data.seasons;
+};
+
+export const getSeasonByIdApi = async (
+    seasonId: string,
+): Promise<Season> => {
+    const response = await api.get(`/seasons/${seasonId}`);
+    console.log(response)
+    return response.data.season;
 };
