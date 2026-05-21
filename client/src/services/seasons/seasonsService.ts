dab7f7f3ef976c0708afc1859d3c9a810d2c2283
Arun Kumar <mymailarun32@gmail.com>
2026-05-21 14:20:51 +0530

    create service for season

--- client/src/services/seasons/seasonsService.ts
+++ client/src/services/seasons/seasonsService.ts
@@ -1,15 +1,36 @@
 // services/season/seasonService.ts
 
-import { getAllSeasonsApi, Season, getSeasonByIdApi } from "@/api/seasonApi";
+import {
+  getAllSeasonsApi,
+  Season,
+  getSeasonByIdApi,
+  createSeasonApi,
+  CreateSeasonPayload,
+  CreateSeasonResponse,
+} from "@/api/seasonApi";
 
 // ======================================================
-// SERVICE
+// GET ALL SEASONS
 // ======================================================
 
 export const getAllSeasons = async (): Promise<Season[]> => {
   return await getAllSeasonsApi();
 };
 
+// ======================================================
+// GET SINGLE SEASON
+// ======================================================
+
 export const getSeasonById = async (seasonId: string): Promise<Season> => {
   return await getSeasonByIdApi(seasonId);
+};
+
+// ======================================================
+// CREATE SEASON
+// ======================================================
+
+export const createSeason = async (
+  data: CreateSeasonPayload,
+): Promise<CreateSeasonResponse> => {
+  return await createSeasonApi(data);
 };
